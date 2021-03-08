690089592e8997cb7273d9e14772cfee9e5304a0
myin <minyin142p@gmail.com>
2021-03-08 18:43:57 +0100

    Matching cards

--- scenes/MemoryCardGrid.gd
+++ scenes/MemoryCardGrid.gd
@@ -1,12 +1,21 @@
-extends GridContainer
+extends Control
+
+signal cards_matched
 
 const POKER_CARD_SCENE = preload("res://assets/poker-cards/PokerCard.tscn")
+
+export var match_count = 4
+
+onready var grid := $GridContainer
+onready var revealed_timer := $RevealedTimer
+
+var revealed_cards = []
 
 func _ready():
 	var cards = create_card_scenes()
 	cards.shuffle()
 	for card in cards:
-		add_child(card)
+		grid.add_child(card)
 
 
 func create_card_scenes() -> Array:
@@ -34,4 +43,35 @@ func create_card_scenes() -> Array:
 
 
 func _on_card_click(card: PokerCard):
+	if _max_revealed_cards():
+		return
+	
 	card.show_back = false
+	revealed_cards.append(card)
+	
+	if _max_revealed_cards():
+		revealed_timer.start()
+
+
+func _max_revealed_cards() -> bool:
+	return revealed_cards.size() >= match_count
+
+
+func _revealed_cards_matching() -> bool:
+	var value = revealed_cards.front().value
+	
+	var card_match = 0
+	for card in revealed_cards:
+		if card.value == value:
+			card_match += 1
+	
+	return card_match == match_count
+
+
+func _on_RevealedTimer_timeout():
+	if _revealed_cards_matching():
+		emit_signal("cards_matched")
+	else:
+		for c in revealed_cards:
+			c.show_back = true
+	revealed_cards = []
